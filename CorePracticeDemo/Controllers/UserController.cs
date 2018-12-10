382edff5ee928bb44799e880ad116d2e785bd10d
aellis21 <andr3w3llis@gmail.com>
2018-12-11 07:24:45 +1100

    Added UserController

--- CorePracticeDemo/Controllers/UserController.cs
+++ CorePracticeDemo/Controllers/UserController.cs
@@ -1,14 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-using System.Web;
+﻿using System.Web.Http;
+using CorePracticeDemo.Services;
+using CorePracticeDemo.Services.Models;
 
 namespace CorePracticeDemo.Controllers
 {
-    [RoutePrefix("Users")]
+    [RoutePrefix("users")]
     public class UserController : ApiController
     {
+        private UserService _userService;
 
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet, Route("all")]
+        public IHttpActionResult GetAll()
+        {
+            return Ok(_userService.GetAll());
+        }
+
+        [HttpGet, Route("{id}")]
+        public IHttpActionResult GetById(int id)
+        {
+            return Ok(_userService.GetById(id));
+        }
+
+        [HttpPost, Route("")]
+        public IHttpActionResult CreateUser(User user)
+        {
+            return Ok(_userService.Create(user));
+        }
+
+        [HttpPut, Route("{id}")]
+        public IHttpActionResult UpdateUser(int id, User user)
+        {
+            user.Id = id;
+
+            return Ok(_userService.Create(user));
+        }
     }
 }
