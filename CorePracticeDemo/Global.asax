d41588092257c84ec5fca0e4f669a7619649757b
aellis21 <andr3w3llis@gmail.com>
2018-12-11 07:53:07 +1100

    Added WebApiConfig, LightInject and ServiceContainer

---
+++ CorePracticeDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tofi.Demo.Api.WebApiApplication" Language="C#" %>
---
+++ CorePracticeDemo/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using LightInject;
+using CorePracticeDemo.Repositories;
+using CorePracticeDemo.Repositories.Interfaces;
+using CorePracticeDemo.Data;
+using CorePracticeDemo.App_Start;
+using CorePracticeDemo.Services;
+using System.Web;
+
+namespace Tofi.Demo.Api
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            var container = new ServiceContainer();
+
+            container.Register<DemoDataContext>(c => new DemoDataContext("connectionString"));
+            container.Register<IUserRepository, UserRepository>();
+            container.Register<UserService, UserService>();
+
+            container.RegisterApiControllers();
+            container.EnableWebApi(GlobalConfiguration.Configuration);
+        }
+    }
+}
